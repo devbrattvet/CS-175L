dfa3b38a4caff354026f364eaf560f2c6f556700
devinbrattvet <devinbrattvet@10.71.51.156>
2021-03-04 22:32:49 -0500

    Bank account v2

--- Bank/src/Bank/bankAccount.java
+++ Bank/src/Bank/bankAccount.java
@@ -5,40 +5,60 @@ applying interest or fees at the end of the month.
 */
 public class bankAccount
 {
-private double balance;
+	 private double balance;
+	 private double interestPercent;
+	 private double interest;
 
-/**
-   Constructs a bank account with zero balance.
-*/
-public bankAccount(double startBal)
-{
-   balance = startBal;
-}
+	   /**
+	      Constructs a bank account with zero balance.
+	   */
+	   public bankAccount(double startBal,double interestPct)
+	   {
+	      balance = startBal;
+	      interestPercent = interestPct;
+	      System.out.println("Created new account with $" + balance + " and interest rate " + interestPct);
+	   }
 
-/**
-   Makes a deposit into this account.
-   @param amount the amount of the deposit
-*/
-public void deposit(double amount)
-{
-   balance = balance + amount;
-}
+	   /**
+	      Makes a deposit into this account.
+	      @param amount the amount of the deposit
+	   */
+	   public void deposit(double amount)
+	   {
+	      balance = balance + amount;
+	      System.out.println("Deposited: $" + amount);
+	   }
 
-/**
-   Makes a withdrawal from this account, or charges a penalty if
-   sufficient funds are not available.
-   @param amount the amount of the withdrawal
-*/
-public void withdraw(double amount)
-{
-   balance = balance - amount;
-}
-/**
-   Gets the current balance of this bank account.
-   @return the current balance
-*/
-public double getBalance()
-{
-   return balance;
-}
-}
+	   /**
+	      Makes a withdrawal from this account, or charges a penalty if
+	      sufficient funds are not available.
+	      @param amount the amount of the withdrawal
+	   */
+	   public void withdraw(double amount)
+	   {
+		  if (amount > balance)
+		  {
+			  System.out.println("Insufficient funds to support withdrawal");
+		  }
+		  else
+		  {
+			  balance = balance - amount;
+			  System.out.println("Withdrew: $" + amount);
+		  }  	 
+	   }
+	   
+	   public void calcInterest()
+	   {
+		  interest=balance*interestPercent;
+		  balance = interest + balance;
+		  System.out.println("Interest: $" + interest);
+	   }
+	   /**
+	      Gets the current balance of this bank account.
+	      @return the current balance
+	   */
+	   public double getBalance()
+	   {
+	      return balance;
+	   }
+	}
